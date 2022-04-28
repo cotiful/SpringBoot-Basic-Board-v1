69a7a9f8ca38afb0d943ca5e5c0ba59ca7edebf8
yuseunghyun <ysh7091@gmail.com>
2022-04-28 20:10:48 +0900

    글 수정페이지 완료

--- src/main/java/site/metacoding/board/web/BoardController.java
+++ src/main/java/site/metacoding/board/web/BoardController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
@@ -22,6 +24,17 @@ import site.metacoding.board.domain.BoardRepository;
 public class BoardController {
 
     private final BoardRepository boardRepository;
+
+    // json 요청을 받을 때는 RequestBody가 필요함
+    @PutMapping("/api/board/{id}")
+    public ResponseEntity<?> updateBoard(@PathVariable Integer id, @RequestBody Board board) {
+        board.setId(id);
+        boardRepository.save(board);
+
+        // body 부분이 object 이면 Json, 그냥 문자열이면 text
+        return new ResponseEntity<>("수정잘됨", HttpStatus.OK);
+
+    }
 
     // data 를 리턴하는 함수이다. 해당하는 함수는 reponseEntity 가 붙어서 Json을 리턴한다.
     // ViewResolver가 발동하지 않는다. @ ViewResovler 란 @controller 가 붙어 있을떄 작동한다
@@ -53,8 +66,14 @@ public class BoardController {
         return "boardWriteForm";
     }
 
-    @GetMapping("/board/updateForm")
-    public String boardUpdateForm() {
+    @GetMapping("/board/{id}/updateForm")
+    public String boardUpdateForm(@PathVariable Integer id, Model model) {
+        Optional<Board> boardOp = boardRepository.findById(id);
+        if (boardOp.isPresent()) {
+            model.addAttribute("board", boardOp.get());
+        } else {
+            return "error";
+        }
         return "boardUpdateForm";
     }
 

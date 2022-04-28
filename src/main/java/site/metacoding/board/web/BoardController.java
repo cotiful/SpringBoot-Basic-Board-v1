de77bbacaf4ca6aae9664da1ef59164d91a7c48d
yuseunghyun <ysh7091@gmail.com>
2022-04-28 19:51:05 +0900

    글 삭제하기

--- src/main/java/site/metacoding/board/web/BoardController.java
+++ src/main/java/site/metacoding/board/web/BoardController.java
@@ -2,8 +2,11 @@ package site.metacoding.board.web;
 
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +22,14 @@ import site.metacoding.board.domain.BoardRepository;
 public class BoardController {
 
     private final BoardRepository boardRepository;
+
+    // data 를 리턴하는 함수이다. 해당하는 함수는 reponseEntity 가 붙어서 Json을 리턴한다.
+    // ViewResolver가 발동하지 않는다. @ ViewResovler 란 @controller 가 붙어 있을떄 작동한다
+    @DeleteMapping("/api/board/{id}")
+    public ResponseEntity<?> deleteBoard(@PathVariable Integer id) {
+        boardRepository.deleteById(id);
+        return new ResponseEntity<>("삭제잘됨", HttpStatus.OK);
+    }
 
     @GetMapping({ "/", "/board" })
     public String boardList(Model model) {
